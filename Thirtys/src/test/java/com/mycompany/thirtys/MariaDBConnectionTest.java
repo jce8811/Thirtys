f47c4fb8211be1ab57a88d2eea4409ec07cb0d5a
emma463 <emma463@naver.com>
2019-11-25 01:04:44 +0900

    19.11.25 // 마리아db 연결확인

---
+++ Thirtys/src/test/java/com/mycompany/thirtys/MariaDBConnectionTest.java
@@ -0,0 +1,24 @@
+package com.mycompany.thirtys;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.jupiter.api.Test;
+
+public class MariaDBConnectionTest {
+	
+	private static final String DRIVER = "org.mariadb.jdbc.Driver";
+	private static final String URL = "jdbc:mariadb://127.0.0.1:3306/thirtys";
+	private static final String USER = "root";
+	private static final String PASSWORD = "8811";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);) {
+			System.out.println(conn);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
