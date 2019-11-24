76219e8f72890759893b55a0fc3fcb998747766d
emma463 <emma463@naver.com>
2019-11-25 02:22:26 +0900

    19.11.25 로그백 설정

--- Thirtys/src/test/java/com/mycompany/thirtys/MariaDBConnectionTest.java
+++ Thirtys/src/test/java/com/mycompany/thirtys/MariaDBConnectionTest.java
@@ -4,11 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 
 public class MariaDBConnectionTest {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MariaDBConnectionTest.class);
 	private static final String DRIVER = "org.mariadb.jdbc.Driver";
 	private static final String URL = "jdbc:mariadb://127.0.0.1:3306/thirtys";
 	private static final String USER = "root";
@@ -19,6 +22,8 @@ public class MariaDBConnectionTest {
 		Class.forName(DRIVER);
 		try(Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);) {
 			System.out.println(conn);
+			System.out.println("연결되었습니다.");
+			logger.info("연결되었습니다.");
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
